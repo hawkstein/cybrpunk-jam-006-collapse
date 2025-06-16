ef42067d70e6108904b0a44f834211480555df7a
Liam Hawks <68058931+hawkstein@users.noreply.github.com>
2025-06-16 20:06:30 +0100

    improve start title outro

--- main/start_screen.gd
+++ main/start_screen.gd
@@ -4,11 +4,13 @@ extends Control
 
 func _on_start_button_pressed() -> void:
 	start_button.visible = false
-	animation_player.play(&"blurb")
+	animation_player.play(&"outro")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == &"intro":
 		start_button.visible = true
+	if anim_name == &"outro":
+		animation_player.play(&"blurb")
 	if anim_name == &"blurb":
 		var fade_opts = SceneManager.create_options()
 		var no_click_opts = SceneManager.create_general_options(Color(0,0,0), 0, false)
